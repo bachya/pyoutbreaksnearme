dbe32591665ddec5a980dabc09973ab0f0f272e6
Aaron Bach <bachya1208@gmail.com>
2022-08-03 16:11:20 -0600

    Fix bug with CDC data where coordinates are missing (#4)

--- examples/test_client.py
+++ examples/test_client.py
@@ -1,7 +1,6 @@
 """Test client functionality."""
 import asyncio
 import logging
-import os
 
 from aiohttp import ClientSession
 
@@ -10,24 +9,26 @@ from pyoutbreaksnearme.errors import OutbreaksNearMeError
 
 _LOGGER = logging.getLogger()
 
-LATITUDE = os.getenv("LATITUDE")
-LONGITUDE = os.getenv("LONGITUDE")
-
 
 async def main() -> None:
     """Create the aiohttp session and run the example."""
     async with ClientSession() as session:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.DEBUG)
 
         client = Client(session=session)
         try:
             nearest_user_data = await client.user_data.async_get_nearest_by_coordinates(
-                LATITUDE, LONGITUDE
+                39.7251035, -104.99918
             )
             _LOGGER.info("Nearest user data: %s", nearest_user_data)
 
             user_totals_data = await client.user_data.async_get_totals()
             _LOGGER.info("User data totals: %s", user_totals_data)
+
+            nearest_cdc_data = await client.cdc_data.async_get_nearest_by_coordinates(
+                39.7251035, -104.99918
+            )
+            _LOGGER.info("Nearest CDC data: %s", nearest_cdc_data)
         except OutbreaksNearMeError as err:
             _LOGGER.error(err)
 
